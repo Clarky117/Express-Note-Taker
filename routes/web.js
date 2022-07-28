9db5355fdb65e01c8c6591c481f6268dadf2b008
Clarky <clarky117@outlook.com>
2022-07-28 11:39:52 +0800

    files created, small pseudocode written, imported express and router instance

---
+++ routes/web.js
@@ -0,0 +1,11 @@
+// create router instance using express
+const router = require('express').Router();
+
+// * `GET /` should return the `index.html` file.
+// * `GET /notes` should return the `notes.html` file.
+// * `GET *` should return a 404 error.
+
+
+
+// export router
+module.exports = router;
